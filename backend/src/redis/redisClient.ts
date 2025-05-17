2fcfabbd127347fbe4e1df1424466d40c3266ad7
Yuriy-BilykB <urabilik90@gmail.com>
2025-05-17 19:06:37 +0300

    Commit change app

--- backend/src/redis/redisClient.ts
+++ backend/src/redis/redisClient.ts
@@ -1,22 +1,14 @@
 import { createClient } from 'redis';
-const host = process.env.REDIS_HOST!;
-const port = process.env.REDIS_PORT!;
-const password = process.env.REDIS_PASSWORD!;
 
-
-if (!host || !port || !password) {
-    throw new Error("❌ REDIS environment variables are not set");
-}
 const redisClient = createClient({
     socket: {
-        host: host,
-        port: Number(port),
-        tls: true
+        host: "youary-shop-redis",
+        port: 6379,
+        tls: false,
     },
-    password
 });
 
-redisClient.on('error', (err) => console.error('Redis Error:', err));
+redisClient.on('error', (err) => console.error('❌ Redis Error:', err));
 
 (async () => {
     try {
