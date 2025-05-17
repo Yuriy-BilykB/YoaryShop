230cf4bdb8991548b4a96d91b0f256841a0a047f
Yuriy-BilykB <urabilik90@gmail.com>
2025-05-17 19:51:28 +0300

    Commit change app

--- backend/src/redis/redisClient.ts
+++ backend/src/redis/redisClient.ts
@@ -1,22 +1,22 @@
-import { createClient } from 'redis';
-
-const redisClient = createClient({
-    socket: {
-        host: "youary-shop-redis.internal.delightfulwave-daf5feb2.polandcentral.azurecontainerapps.io",
-        port: 6379,
-        tls: false,
-    },
-});
-
-redisClient.on('error', (err) => console.error('❌ Redis Error:', err));
-
-(async () => {
-    try {
-        await redisClient.connect();
-        console.log('✅ Redis connected');
-    } catch (err) {
-        console.error('❌ Redis connection error:', err);
-    }
-})();
-
-export { redisClient };
+// import { createClient } from 'redis';
+//
+// const redisClient = createClient({
+//     socket: {
+//         host: "youary-shop-redis.internal.delightfulwave-daf5feb2.polandcentral.azurecontainerapps.io",
+//         port: 6379,
+//         tls: false,
+//     },
+// });
+//
+// redisClient.on('error', (err) => console.error('❌ Redis Error:', err));
+//
+// (async () => {
+//     try {
+//         await redisClient.connect();
+//         console.log('✅ Redis connected');
+//     } catch (err) {
+//         console.error('❌ Redis connection error:', err);
+//     }
+// })();
+//
+// export { redisClient };
